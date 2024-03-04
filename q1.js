cf8bc10a7009d9f02719c822756324bc7c73b014
Ram <ram440163@gmail.com>
2024-03-04 10:27:55 +0530

    s

---
+++ q1.js
@@ -0,0 +1,17 @@
+// Info about http 1.1
+
+// The client browser has to send a request to the server using
+// the method (GET/POST).
+// loads resources one after the other,one resource cannot be 
+// loaded,it blocks all the other resources behind it.
+// Tcp connection to request needed for
+// ,Tcp 1,Tcp 2,Tcp 3 connection request to server.
+
+
+// Info about http 2
+
+// Http 2 supports full multiplexing for requests as well as responses
+// over the single tcp connection.
+// This capabilities,lower page load times are achieved by removing'
+// needless and improving the network .
+// Http 2 is using single Tcp connection to send multiple request to server
---
+++ q2.js
@@ -0,0 +1,3 @@
+// object is key value array going on to index value 
+// object -name, age
+//         "ram"  24 
